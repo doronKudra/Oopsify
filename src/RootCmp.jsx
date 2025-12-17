26f5565f1a5f24b8cd075d0ad4229d0c1b5334ff
doronKudra <doron0505@gmail.com>
2025-12-17 14:37:43 +0200

    App Skeleton

---
+++ src/RootCmp.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Routes, Route } from 'react-router'
+
+import { HomePage } from '../archive/HomePage.jsx'
+import { AboutUs, AboutTeam, AboutVision } from '../archive/AboutUs.jsx'
+import { StationIndex } from './pages/StationIndex.jsx'
+
+import { ChatApp } from '../archive/Chat.jsx'
+import { AdminIndex } from '../archive/AdminIndex.jsx'
+
+import { StationDetails } from '../archive/StationDetails.jsx'
+import { UserDetails } from './pages/UserDetails'
+
+import { AppHeader } from './cmps/AppHeader'
+import { AppFooter } from './cmps/AppFooter'
+import { UserMsg } from './cmps/UserMsg.jsx'
+import { LoginSignup } from './pages/LoginSignup.jsx'
+import { Login } from './pages/Login.jsx'
+import { Signup } from './pages/Signup.jsx'
+
+export function RootCmp() {
+    return (
+        <div className="main-container">
+            <AppHeader />
+            <UserMsg />
+
+            <main>
+                <Routes>
+                    <Route path="" element={<HomePage />} />
+                    <Route path="about" element={<AboutUs />}>
+                        <Route path="team" element={<AboutTeam />} />
+                        <Route path="vision" element={<AboutVision />} />
+                    </Route>
+                    <Route path="station" element={<StationIndex />} />
+                    <Route path="station/:stationId" element={<StationDetails />} />
+                    <Route path="user/:id" element={<UserDetails />} />
+                    <Route path="chat" element={<ChatApp />} />
+                    <Route path="admin" element={<AdminIndex />} />
+                    <Route path="login" element={<LoginSignup />}>
+                        <Route index element={<Login />} />
+                        <Route path="signup" element={<Signup />} />
+                    </Route>
+                </Routes>
+            </main>
+            <AppFooter />
+        </div>
+    )
+}
+
+
