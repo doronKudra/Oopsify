caaf951a9d45c3131cf2463b67b3411197b82371
Zohar <zoharar4@gmail.com>
2025-12-31 13:41:51 +0200

    play button in index

--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom'
 import { FastAverageColor } from 'fast-average-color'
 import { useSelector } from 'react-redux'
+import { PlayIcon } from './icons/PlayIcon'
+import { PauseIcon } from './icons/PauseIcon'
+import { playerActions } from '../store/actions/player.actions'
 
 export function StationPreview({
     openContextMenu,
@@ -18,6 +21,7 @@ export function StationPreview({
             context: { station },
         })
     }
+
     return (
         <DynamicCmp
             onStationRightClick={onStationRightClick}
@@ -41,6 +45,21 @@ function DynamicCmp({ onStationRightClick, station, listType, onHoverColor }) {
 
         onHoverColor?.(color)
     }
+
+
+    function onPlayStation(station, ev) {
+        ev.stopPropagation()
+        ev.preventDefault()
+        if (isThisStationPlaying) {
+            playerActions.onPlaying(false)
+        } else if (currStationId !== station._id) {
+            playerActions.onPlayStation(station)
+        } else {
+            playerActions.onPlaying(true)
+        }
+
+    }
+
     const currStationId = useSelector((state) => state.playerModule.stationId)
     const isPlayerPlaying = useSelector((state) => state.playerModule.isPlaying)
     const isThisStationPlaying =
@@ -70,13 +89,13 @@ function DynamicCmp({ onStationRightClick, station, listType, onHoverColor }) {
                             </span>
                             {
                                 !(station._id === 'liked-tracks') &&
-                                    station?.owner?.name && (
-                                        <span className="sidebar-preview-station-info">
-                                            {'Playlist' +
-                                                ' • ' +
-                                                station?.owner?.name}
-                                        </span>
-                                    ) //station.type pinned svg: <svg data-encore-id="icon" role="img" aria-hidden="false" class="e-91000-icon e-91000-baseline wJ1guHZhFtkqK3QIfpqy" viewBox="0 0 16 16" style="--encore-icon-fill: var(--text-bright-accent, #107434); --encore-icon-height: var(--encore-graphic-size-informative-smaller-2); --encore-icon-width: var(--encore-graphic-size-informative-smaller-2);"><title>Pinned</title><path d="M8.822.797a2.72 2.72 0 0 1 3.847 0l2.534 2.533a2.72 2.72 0 0 1 0 3.848l-3.678 3.678-1.337 4.988-4.486-4.486L1.28 15.78a.75.75 0 0 1-1.06-1.06l4.422-4.422L.156 5.812l4.987-1.337z"></path></svg>
+                                station?.owner?.name && (
+                                    <span className="sidebar-preview-station-info">
+                                        {'Playlist' +
+                                            ' • ' +
+                                            station?.owner?.name}
+                                    </span>
+                                ) //station.type pinned svg: <svg data-encore-id="icon" role="img" aria-hidden="false" class="e-91000-icon e-91000-baseline wJ1guHZhFtkqK3QIfpqy" viewBox="0 0 16 16" style="--encore-icon-fill: var(--text-bright-accent, #107434); --encore-icon-height: var(--encore-graphic-size-informative-smaller-2); --encore-icon-width: var(--encore-graphic-size-informative-smaller-2);"><title>Pinned</title><path d="M8.822.797a2.72 2.72 0 0 1 3.847 0l2.534 2.533a2.72 2.72 0 0 1 0 3.848l-3.678 3.678-1.337 4.988-4.486-4.486L1.28 15.78a.75.75 0 0 1-1.06-1.06l4.422-4.422L.156 5.812l4.987-1.337z"></path></svg>
                             }
                             {station._id === 'liked-tracks' && (
                                 <span className="sidebar-preview-station-info">
@@ -93,10 +112,18 @@ function DynamicCmp({ onStationRightClick, station, listType, onHoverColor }) {
                 <article onContextMenu={(ev) => onStationRightClick(ev, station)} className="index-station-preview-container">
                     <div className="medium-img-container">
                         <img className="medium-img" src={station?.images?.at(0)?.url}></img>
-                        <button className='station-index-play-btn'>
-                            <svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 24 24"><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606"></path></svg>
+                        <button
+                            onClick={ev => onPlayStation(station, ev)}
+                            className={`station-index-play-btn ${isThisStationPlaying ? 'playing' : ''}`}>
+                            {isThisStationPlaying
+                                ?
+                                <PauseIcon width={24} height={24} />
+                                :
+                                <PlayIcon width={24} height={24} />
+                            }
                         </button>
                     </div>
+
                     <div className="index-station-preview">
                         <span className="index-station-preview-title">
                             {station?.name?.slice(0, 25)}
@@ -108,7 +135,7 @@ function DynamicCmp({ onStationRightClick, station, listType, onHoverColor }) {
                     </div>
 
                 </article>
-            </Link>
+            </Link >
         case 'recent': // small image, title
             return (
                 <Link
